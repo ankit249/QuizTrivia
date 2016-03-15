e86384fbbaad789c19f4a3c0b98e0004d375532e
Ankit Shah <shahankit@gmail.com>
2016-03-14 18:01:35 -0700

    Trivia1

---
+++ QuizTrivia/src/Trivia1.java
@@ -0,0 +1,40 @@
+
+class A {
+	int count = 0;
+
+	public synchronized void increament() throws InterruptedException {
+		Thread.sleep(100);
+		for (int i = 0; i < 10000; i++) {
+			count++;
+		}
+	}
+}
+
+public class Trivia1 {
+	public static void main(String[] args) {
+		final A a = new A();
+		Thread t1 = new Thread(new Runnable() {
+			public void run() {
+				try {
+					a.increament();
+				} catch (InterruptedException ie) {
+				}
+			}
+		});
+
+		Thread t2 = new Thread(new Runnable() {
+			public void run() {
+				try {
+					a.increament();
+				} catch (InterruptedException ie) {
+				}
+			}
+		});
+
+		t1.start();
+		t2.start();
+
+
+		System.out.println(a.count);
+	}
+}
