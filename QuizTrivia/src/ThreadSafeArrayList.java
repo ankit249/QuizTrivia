a61db1bba57a0b856c8ecffbabc1889798fcbb53
ankit249 <shahankit@gmail.com>
2016-03-17 13:45:40 -0700

    Make this ArrayList ThreadSafe

---
+++ QuizTrivia/src/ThreadSafeArrayList.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ThreadSafeArrayList<T> {
+
+	private final List<T> list = new ArrayList<T>();
+
+	public void set(T o) {
+		list.add(o);
+		System.out.println("Adding element by thread" + Thread.currentThread().getName());
+	}
+
+	public T get(int i) {
+		System.out.println("Printing elements by thread" + Thread.currentThread().getName());
+		return list.get(i);
+	}
+
+	public static void main(String[] args) {
+		ThreadSafeArrayList<String> threadSafeArrayList = new ThreadSafeArrayList<>();
+		threadSafeArrayList.set("1");
+		threadSafeArrayList.set("2");
+		threadSafeArrayList.set("3");
+
+		System.out.println("Printing the First Element : " + threadSafeArrayList.get(1));
+	}
+}
